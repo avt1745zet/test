9d6c29826c665f5fb332b256280f8c3e4587bbee
YY <yang0yu0yy@gmail.com>
2019-06-21 23:53:45 +0800

    test

---
+++ res/js/test.js
@@ -0,0 +1,12 @@
+/*
+     * popup-youtube
+     */
+$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: true
+});
